f1296958eb3bf6fbeb8f28b18aca50be0880f0a0
wangha <wanghamax@gmail.com>
2024-12-03 05:12:19 -0500

    * NEW log.h is support.
    Signed-off-by: wangha <wanghamax@gmail.com>

---
+++ inc/log.h
@@ -0,0 +1,20 @@
+#ifndef PARQUET_TOOL_LOG_H
+#define PARQUET_TOOL_LOG_H
+
+#if 0
+
+#define ptlog(format, ...) \
+	do {                    \
+	} while (0)
+
+#else
+
+#define ptlog(format, arg...)                                                 \
+	do {                                                                  \
+		fprintf(stderr, "%s:%d(%s) " format "\n", __FILE__, __LINE__, \
+		    __FUNCTION__, ##arg);                                     \
+	} while (0)
+
+#endif
+
+#endif
