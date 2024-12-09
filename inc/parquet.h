f884449c1b2d3ab71f010871266fa6cbf503110b
wangha <wanghamax@gmail.com>
2024-12-09 01:41:13 -0500

    * NEW search function is introduced.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- inc/parquet.h
+++ inc/parquet.h
@@ -5,5 +5,7 @@ void pt_binary(char *col, int argc, char **argv);
 void pt_decrypt(char *col, char *footkey, char *col1key, char *col2key, int argc, char **argv);
 void pt_replay(char *interval, char *url, char *topic, int argc, char **argv);
 void pt_decreplay(char *footkey, char *col1key, char *col2key, char *interval, char *url, char *topic, int argc, char **argv);
+void pt_search(char *col, char *signal, char *start_key, char *end_key, char *dir);
+void pt_decsearch(char *footkey, char *col1key, char *col2key, char *col, char *signal, char *start_key, char *end_key, char *dir);
 
 #endif
