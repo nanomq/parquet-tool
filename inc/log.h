cfa671f02def88095b7f841399510adc7618d6fc
wangha <wanghamax@gmail.com>
2025-01-03 02:26:24 -0500

    * NEW Add log level pterr.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- inc/log.h
+++ inc/log.h
@@ -8,16 +8,26 @@ void ptlog_init();
 
 #if 0
 
+#define pterr(format, ...) \
+	do {                    \
+	} while (0)
+
 #define ptlog(format, ...) \
 	do {                    \
 	} while (0)
 
 #else
 
+#define pterr(format, arg...)                                                 \
+	do { \
+		fprintf(stderr, "%s:%d(%s) " format "\n", __FILE__, __LINE__, \
+		    __FUNCTION__, ##arg);                                     \
+	} while (0)
+
 #define ptlog(format, arg...)                                                 \
 	do { \
 		if (!is_quiet_mode()) { \
-			fprintf(stderr, "%s:%d(%s) " format "\n", __FILE__, __LINE__, \
+			fprintf(stdout, "%s:%d(%s) " format "\n", __FILE__, __LINE__, \
 			    __FUNCTION__, ##arg);                                     \
 		} \
 	} while (0)
