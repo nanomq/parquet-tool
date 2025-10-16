a4e96b9e2f5b8e34df8d429e25517de0c96629b2
wangha <wanghamax@gmail.com>
2025-10-16 05:35:06 -0400

    * RM the unit test test_r_schema_parquet.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- src/test_parquet.cpp
+++ src/test_parquet.cpp
@@ -11,30 +11,6 @@
 using namespace std;
 
 void
-test_r_schema_parquet()
-{
-	char *output = (char *)"./test_schema.parquet";
-	map<string, any> m = read_parquet_schema(output, NULL, 0);
-	list<int64_t> lk = any_cast<list<int64_t>>(m["key"]);
-	vector<vector<string>> larr = any_cast<vector<vector<string>>>(m["schemadata"]);
-	list<int64_t>::iterator lk_it = lk.begin();
-	for (int i=0; i<larr.size(); ++i) {
-		vector<string>& pld = larr[i];
-		printf("%lld", *lk_it);
-		lk_it++;
-		for (auto it = pld.begin(); it != pld.end(); it ++) {
-			printf(", ");
-			string& ele = *it;
-			for (int n=0; n<ele.size(); ++n)
-				printf("%02x", (uint8_t)ele.c_str()[n]);
-			if (ele.size() == 0)
-				printf("-");
-		}
-		printf("\n");
-	}
-}
-
-void
 test_rw_parquet()
 {
 	char *output = (char *)"./test.parquet";
@@ -122,7 +98,6 @@ test_fuzz()
 int
 main()
 {
-	test_r_schema_parquet();
 	test_rw_parquet();
 	test_search();
 	test_fuzz();
